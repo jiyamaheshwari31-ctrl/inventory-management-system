a325a9d372c45a97188fb2619ac823de0daa60c7
Jiya <jiyamaheshwari31@gmail.com>
2026-09-08 22:56:45 +0530

    thoda sa frontend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -3,7 +3,7 @@
 // =========================================================
 
 // Your backend already contains /api in its base path.
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = "https://inventory-management-system-c5tr.onrender.com";
 
 
 function getToken() {
